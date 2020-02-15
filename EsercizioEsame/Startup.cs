029062d8ebc4ecbcf3a680616bd29aee643b7b91
Nicola Piermattei <pierma.nick@live.it>
2020-02-15 09:25:39 +0100

    Push Iniziale

---
+++ EsercizioEsame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EsercizioEsame.Startup))]
+namespace EsercizioEsame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
